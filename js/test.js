d49c50cb357810a26ecac0102690c2d86ae19592
Peng <G00267682@stu.raritanval.edu>
2018-11-14 19:06:56 -0500

    apparently about 20% of the time the loader in .\js\ds-test.js works, but the other 80% of the time it fails (silently) with the message "Failed to load element using: IMG at t.abort (./js/pixi.min.js:9:17165) at t._onError (...)."

--- js/test.js
+++ js/test.js
@@ -1,8 +1,31 @@
-var test = new Solar.Scene("test");
+function doSolar(){
+    var loader=new PIXI.loaders.Loader();
+    
+    loader.add("img/sun.png")/*.add(ehh).add(ffff)*/;
+    
+    loader.load((loader, resources)=>{
+        //console.log(resources);
+        var newSun=new PIXI.Sprite(resources["img/sun.png"].texture);
+        newSun.width=400;
+        newSun.height=400;
+        newSun.x=0;
+        newSun.y=0;
+        newSun.name="stop failing";
+        newSun.anchor.set(0.5);
+        app.stage.addChild(newSun);
+        
+        console.log("done")
+        
+        var test = new Solar.Scene("testdotjs");
+        test.addChild(newSun);
+        
+        app.stage.addChild(newSun);
+    });
 
-var theSun2 = PIXI.Sprite.fromImage("img/sun.png");
-            theSun2.width = 400;
-            theSun2.height = 400;
-            theSun2.anchor.set(0.5);
+    /*var theSun2 = PIXI.Sprite.fromImage();
+                theSun2.width = 400;
+                theSun2.height = 400;
+                theSun2.anchor.set(0.5);
 
-test.addChild(theSun2);
+    test.addChild(theSun2);*/
+}
