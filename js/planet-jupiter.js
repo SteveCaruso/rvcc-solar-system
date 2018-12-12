69a10352c229ce3b5f49c49bfa2f9ce5e3930426
DragonhandX <31867624+DragonhandX@users.noreply.github.com>
2018-12-12 18:56:02 -0500

    jupiter moons picture adjusted
    changed size and position, adjusted

--- js/planet-jupiter.js
+++ js/planet-jupiter.js
@@ -43,7 +43,7 @@ Solar.loader.on('complete',function(loader,resources) {
     content.addChild(title);
             
     //Info box text
-    var text = new PIXI.Text("• Day: Just under 10 Earth hours\n\n• Year: 12 Earth Years\n\n• Distance from Sun: 484 million miles \n\n• Size: 86,882 miles in diameter \n\n• Temperatures depend more on the layer \n  of Jupiter's atmosphere: \n  -260 degrees to 1,340 degrees  \n\n• Moons: 79 as of 2018\n  Four largest moons: Io, Europa,\n  Ganymede, and Callisto\n\n• The big red spot is actually a massive \n  storm that is about 10,159 miles across",textStyle);
+    var text = new PIXI.Text("• Day: Just under 10 Earth hours\n\n• Year: 12 Earth Years\n\n• Distance from Sun: 484 million miles \n\n• Size: 86,882 miles in diameter \n\n• Temperatures depend more on the layer \n  of Jupiter's atmosphere: \n  -260 degrees to 1,340 degrees  \n\n• Moons: 79 as of 2018\n  Four largest moons in picture order; Io,\n  Europa, Ganymede, and Callisto\n\n• The big red spot is actually a massive \n  storm that is about 10,159 miles across",textStyle);
         text.x = 1200;
         text.y = 180;
     
@@ -78,10 +78,10 @@ Solar.loader.on('complete',function(loader,resources) {
     
     //Jupiter's moons
     var jmoon = new PIXI.Sprite(resources.jMoon.texture);
-        jmoon.width = 600;
-        jmoon.height = 200;
+        jmoon.width = 900;
+        jmoon.height = 300;
         jmoon.anchor.set(0.5);
-        jmoon.x = 200;
+        jmoon.x = 450;
         jmoon.y = 200;
     
     content.addChild(jmoon);
