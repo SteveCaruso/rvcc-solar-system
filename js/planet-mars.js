b14d11269339b835785fa9f668ba2fac22bf61b3
thejessmooney <54687150+thejessmooney@users.noreply.github.com>
2019-11-14 18:02:28 -0500

    Update planet-mars.js

--- js/planet-mars.js
+++ js/planet-mars.js
@@ -78,8 +78,8 @@ Solar.loader.on('complete',function(loader,resources) {
     scene.addChildAt(planet,1);
     
     var phobos = new PIXI.Sprite(resources.phobos.texture);
-        phobos.width = 300;
-        phobos.height = 300;
+        phobos.width = 225;
+        phobos.height = 225;
         phobos.anchor.set(0.5);
         phobos.x = 150;
         phobos.y = 150;
@@ -107,8 +107,8 @@ Solar.loader.on('complete',function(loader,resources) {
        
         
     var deimos = new PIXI.Sprite(resources.deimos.texture);
-        deimos.width = 275;
-        deimos.height = 275;
+        deimos.width = 225;
+        deimos.height = 225;
         deimos.anchor.set(0.5);
         deimos.x = 1000;
         deimos.y = 400;
