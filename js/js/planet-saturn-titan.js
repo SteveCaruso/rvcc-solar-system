bfe01d701ffa2931e7869532cee504ee4703b055
Livanea <G00258501@stu.raritanval.edu>
2019-11-21 18:18:00 -0500

    Titan...

---
+++ js/js/planet-saturn-titan.js
@@ -0,0 +1,247 @@
+//Create our scene
+var titanScreen = new Solar.Scene("titan");
+
+//here you load the images that you will use on the info box
+//images have to be in the same folde as planets ...\rvcc-solar-system\img
+Solar.loader.add("titan_c","img/titan_c.jpg");
+Solar.loader.add("titan_outline","img/titan_outline.jpg");
+
+//Queue up our files we'll need
+//None!
+
+//When things are loaded, do the stuff necessary to make it work
+Solar.loader.on('complete',function(loader,resources) {
+    
+    //Namespace
+    var scene = titanScreen;
+    var targetPlanet = saturn;
+    
+    //Create a transparent scrim that will be used to fade out the solar system
+    var scrim = new PIXI.Graphics();
+        scrim.beginFill(0x000000);
+        scrim.drawRect(0,0,app.view.width,app.view.height);
+        scrim.alpha = .75;
+    
+    //Add the scrim to the scene
+    scene.addChild(scrim);
+    
+    //Info box
+    var infobox = new PIXI.Sprite(resources.infobox.texture);
+        infobox.width = 750;
+        infobox.height = 1020;
+        infobox.x = 1150;
+        infobox.y = 50;
+    
+    scene.addChild(infobox);
+    
+    
+    //Content container
+    var content = new PIXI.Container();
+        content.alpha = 0;
+    
+    //Info box title
+    var title = new PIXI.Text("Titan",titleStyle);
+        title.x = 1200;
+        title.y = 100;
+    
+    //Add to content
+    content.addChild(title);
+            
+   //Info box text
+   var textTitan = new PIXI.Text("• Name origin: Titan's name comes from Greek mythology. The Titans were elder gods who ruled the universe before the Olympians came to power. \n\n• Orbit: 16 days. \n\n • Size: 1,600 miles in diameter. \n\n• Fun Fact: It is the only know moon with an atmosphere, mostly nitrogen. Also Titan is the most Earth-like place we know of.",textStyle);
+   textTitan.x = 1200;
+   textTitan.y = 180;
+
+    //Add to content
+    content.addChild(textTitan);
+
+    //Add images 
+    //these have to be loaded before hand top of this page (scroll up)
+
+    var img1 = new PIXI.Sprite(resources.titan_c.texture);   
+    
+        img1.width = 216;
+        img1.height = 162;
+        //img1.anchor.set(0.5);
+        img1.x= 1275;
+        img1.y= 780;
+        //img1.filters = [new PIXI.filters.DropShadowFilter()]
+        img1.interactive = true;
+        //img1.on('pointerdown', imageCenter());
+        
+    content.addChild(img1);
+    
+    var img2 = new PIXI.Sprite(resources.titan_outline.texture);   
+    
+        img2.width = 216;
+        img2.height = 162;
+        img2.x= 1565;
+        img2.y= 780;
+
+        //make it interactive
+       //img2.interactive = true;
+        //img2.on('pointerdown', imageCenter());
+        content.addChild(img2);
+
+
+
+    //Add the content container to the scene
+    scene.addChild(content);
+    
+    var backbutton = new PIXI.Sprite(resources.backbutton.texture);
+        backbutton.width = 120;
+        backbutton.height = 120;
+        backbutton.x = 0;
+        backbutton.y = 960;
+        backbutton.alpha = 1;
+    
+    scene.addChild(backbutton);
+    
+    
+    //Create our copy of the Saturn
+    var planet = new PIXI.Sprite(resources.saturn.texture);
+        planet.width = 2160;
+        planet.height = 1080;
+        planet.anchor.set(0.5);
+		planet.x = 540;
+		planet.y = centerY;
+	
+	//Add our Saturn to the scene
+    scene.addChildAt(planet,1);
+    var titan = new PIXI.Sprite(resources.titan.texture);
+        titan.width = 400;
+        titan.height = 400;
+        titan.anchor.set(0.5);
+        titan.x = 200;
+        titan.y = 200;
+   
+    scene.addChild(titan);
+	
+    
+	//Change the default transition
+	scene.transition = async function() {
+        
+        //Set scrim alpha to 0
+        scrim.alpha = 0;
+
+    	//Set alpha to zero
+        scene.alpha = 1;
+
+        //Set infopanel to transparent
+       //infobox.alpha = 0;
+
+        //Add to stage
+        app.stage.addChild(scene);
+        
+        //Fade in
+        //await Animate.to(infobox,500,{alpha:1});
+
+        //Remove old scene
+        app.stage.removeChild(Solar.currentScene);
+
+        //Fix scrim
+        scrim.alpha = .75;
+
+        //Resize panel
+        await Animate.to(infobox,500,{height:1030});
+
+        //Move planet and moon
+        Animate.to(planet,3000,{
+            height:2000,
+            width:4000,
+            x:0,
+            y:0,
+            alpha:.6,
+            easing:Easing.easeInOut
+        });
+        await Animate.to(titan,3000,{
+            height:750,
+            width:750,
+            x:540,
+            y:centerY,
+            easing:Easing.easeInOut
+        });
+
+        //info about Titan
+        await Animate.to(content,1000,{alpha:1,easing:Easing.easeInOut});
+
+        //Fade in content
+        //Here later.
+
+        //Drift the saturn and titan a bit
+        Animate.to(titan,10000,{
+            x:600,
+            easing:Easing.easeInOut
+        });
+		
+	}
+	
+
+	//Change the transition out.
+	scene.transitionOut = async function() {
+        
+        //Move them back!
+        await Animate.to(content,500,{alpha:0,easing:Easing.easeInOut});
+
+        Animate.to(infobox,500,{height:1020});
+        
+        Animate.to(planet,3000,{
+            height:1080,
+            width:2160,
+            x:540,
+            y:centerY,
+            alpha:1,
+            easing:Easing.easeInOut
+        }); 
+        
+        await Animate.to(titan,3000,{
+            height:400,
+            width:400,
+            x:200,
+            y:200,
+            easing:Easing.easeInOut
+        });
+
+        //Fix panel
+        Animate.to(infobox,500,{height:1020});
+
+        //Fix scrim
+        scrim.alpha = 0;
+
+        //Add scene
+        app.stage.removeChild(Solar.incomingScene);
+
+        //Remove old scene
+        app.stage.removeChild(scene);
+
+        Solar.startScene('saturn');
+    }
+    
+    /*
+	//Set the event listeners
+	targetPlanet.interactive = true;
+    targetPlanet.on('pointerdown', function() {
+		Solar.changeSceneTo("earth");
+	});
+	*/
+	//When done, head back
+    backbutton.interactive = true;
+	backbutton.on('pointerdown', async function() {
+        //Set it to false to prevent mashing
+        backbutton.interactive = false;
+
+        //Transition out
+        await scene.transitionOut();
+  
+        //Re-enable button
+        backbutton.interactive = true;
+	});
+    
+    
+    function imageCenter() {
+        img1.width = 800;
+        img1.height = 600;
+        img1.x=  app.view.width/2;
+        img1.y = app.view.height/2;
+    }
+});
---
+++ js/js/planet-saturn.js
@@ -0,0 +1,218 @@
+//Create our scene
+var saturnScreen = new Solar.Scene("saturn");
+
+
+//Load in media
+Solar.loader.add("titan",'img/titan(saturn).png');
+
+
+//When things are loaded, do the stuff necessary to make it work
+Solar.loader.on('complete',function(loader,resources) {
+    
+    //Namespace
+    var scene = saturnScreen;
+    var targetPlanet = saturn;
+    
+    //Create a transparent scrim that will be used to fade out the solar system
+    var scrim = new PIXI.Graphics();
+        scrim.beginFill(0x000000);
+        scrim.drawRect(0,0,app.view.width,app.view.height);
+        scrim.alpha = .75;
+    
+    //Add the scrim to the scene
+    scene.addChild(scrim);
+    
+    //Info box
+    var infobox = new PIXI.Sprite(resources.infobox.texture);
+        infobox.width = 750;
+        infobox.height = 1020;
+        infobox.x = 2000;
+        infobox.y = 50;
+    
+    scene.addChild(infobox);
+    
+    
+    
+    //Content container
+    var content = new PIXI.Container();
+        content.alpha = 0;
+    
+    //Info box title
+    var title = new PIXI.Text("Saturn",titleStyle);
+        title.x = 1200;
+        title.y = 100;
+    
+    //Add to content
+    content.addChild(title);
+            
+    //Info box text
+    var text = new PIXI.Text("• Day: 10.7 Earth Days \n\n• Year: 29 Earth Years \n\n• Size: 74,897 miles in diameter \n\n• Distance from Sun: 886 million miles \n\n• Avg. Temperature:\n  about -288 degrees \n\n• Atmosphere: Hydrogen, Helium \n\n• The rings are mostly made of\n  ice and rock \n\n• Moons: 82 (Titan in Display)",textStyle);
+        text.x = 1200;
+        text.y = 180;
+    
+    //Add to content
+    content.addChild(text);
+    
+    //Add the content container to the scene
+    scene.addChild(content);
+    
+    var backbutton = new PIXI.Sprite(resources.backbutton.texture);
+        backbutton.width = 120;
+        backbutton.height = 120;
+        backbutton.x = 0;
+        backbutton.y = 960;
+        backbutton.alpha = 0;
+    
+    scene.addChild(backbutton);
+    
+    
+    //Create our copy of the saturn
+    var planet = new PIXI.Sprite(resources.saturn.texture);
+        planet.width = 200;
+        planet.height = 200;
+        planet.anchor.set(0.5);
+		planet.x = 0;
+		planet.y = 0;
+	
+	//Add our Saturn to the scene
+    scene.addChildAt(planet,1);
+	
+    //Saturn's moons
+    var titan = new PIXI.Sprite(resources.titan.texture);
+        titan.width = 400;
+        titan.height = 400;
+        titan.anchor.set(0.5);
+        titan.x = 200;
+        titan.y = 200;
+        
+        titan.interactive = false;
+        titan.on('pointerdown', function() {
+            //Make it inactive so no mashing
+            titan.interactive = false;
+            //Start scene change
+            Solar.changeSceneTo("titan");
+
+            //After cooldown period, re-enable interaction
+            setTimeout(() => {
+                titan.interactive = true;
+            },3000);
+        });
+        
+    
+    content.addChild(titan)
+    
+	//Change the default transition
+	scene.transition = async function() {
+		
+        //Stop clickability
+        deactivatePlanets();
+        hideSlider();
+        
+		//If it's from the idle scene, we do something special
+		//if (Solar.currentScene.name == "idle") {
+            
+            //While the real saturn is on the stage, let's grab its coordinates
+            var planetPos = targetPlanet.getGlobalPosition();
+            //And set our saturn to them
+            planet.x = planetPos.x;
+            planet.y = planetPos.y;
+            planet.width = targetPlanet.width * targetPlanet.parent.scale.x;
+            planet.height = targetPlanet.height * targetPlanet.parent.scale.y;
+            
+            //Let's fade in our scene over the idle screen
+            scene.alpha = 0;
+            app.stage.addChild(scene);
+            await Animate.to(scene,1000,{ alpha:1,
+                                                easing:Easing.easeInOut});
+            
+            //Then let's animate the planet growing and filling the screen while we fade out
+            //the solar system
+            Animate.to(planet,3000,{      x:540,
+                                            y:centerY,
+                                            width:2160,
+                                            height:1080,
+                                            easing:Easing.easeInOut
+                                     });
+            
+            Animate.to(solarSystem,3000,{easing:Easing.easeInOut,
+                                              alpha:0});
+            await Animate.to(infobox,3000,{x:1150,y:50,easing:Easing.easeInOut});
+            
+            await Animate.to(content,1000,{alpha:1,easing:Easing.easeInOut});
+        
+            await Animate.to(backbutton,500,{alpha:1});
+            backbutton.interactive = true;
+            titan.interactive = true;
+
+            //Remove the previous scene from the stage and reset it
+            app.stage.removeChild(solarSystem);
+            solarSystem.alpha = 1;
+            
+            
+			
+		//}
+		//Otherwise we do something boring
+		
+		
+	}
+	
+
+	//Change the transition out.
+	scene.transitionOut = async function() {
+		
+        backbutton.interactive = false;
+        titan.interactive = false;
+
+        //While the real saturn is on the stage, let's grab its coordinates
+        var planetPos = targetPlanet.getGlobalPosition();
+        
+        Animate.to(backbutton,500,{alpha:0,
+                                   easing:Easing.easeInOut});
+        
+        await Animate.to(content,500,{alpha:0,easing:Easing.easeInOut});
+        
+        
+        await Animate.to(infobox,1500,{x:2000,y:50,easing:Easing.easeInOut});
+        
+        //Animate solar system back in
+        solarSystem.alpha = 0;
+        app.stage.addChildAt(solarSystem,1);
+        Animate.to(solarSystem,3000,{alpha:1,easing:Easing.easeInOut});
+        
+        //Animate the saturn back.
+        await Animate.to(planet,3000,{    x:planetPos.x,
+                                            y:planetPos.y,
+                                            width:targetPlanet.width * targetPlanet.parent.scale.x,
+                                            height:targetPlanet.height * targetPlanet.parent.scale.y,
+                                            easing:Easing.easeInOut
+                                     });
+        
+        //Fade out the scene
+        await Animate.to(scene,1000,{ alpha:0,
+                                            easing:Easing.easeInOut});
+        
+        
+        
+        //Remove it from the stage
+        app.stage.removeChild(scene);
+        
+        //Make them clickable again
+        activatePlanets();
+        showSlider();
+        
+	}
+	
+	//Set the event listeners
+	targetPlanet.interactive = true;
+    targetPlanet.on('pointerdown', function() {
+		Solar.changeSceneTo("saturn");
+	});
+	
+	//When done, head back
+    backbutton.interactive = false;
+	backbutton.on('pointerdown', function() {
+		Solar.changeSceneTo('idle');
+	});
+	
+	
+});
